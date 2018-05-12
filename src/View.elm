14f43536c60813107cac7a6e660ab0006c5bac0e
Justin Love <git@JustinLove.name>
2018-05-12 12:22:15 -0500

    convert to day offset ranges

--- src/View.elm
+++ src/View.elm
@@ -1,8 +1,12 @@
 module View exposing (Msg(..), view)
 
+import Twitch.Deserialize exposing (Video)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Date exposing (Date)
+import Time exposing (Time)
 
 type Msg
   = None
@@ -15,6 +19,28 @@ view model =
     [ node "style" [] [ text css ]
     , text "view"
     , ul []
-      <| (model.videos
-      |> List.map (\video -> li [] [text <| toString video.duration]))
+      <| (toRanges model.videos
+      |> List.map (\(start, end) -> li []
+      [ text <| toString start
+      , text " - "
+      , text <| toString end
+      ]))
     ]
+
+toRanges : List Video -> List (Time, Time)
+toRanges =
+  List.map toRange
+
+toRange : Video -> (Time, Time)
+toRange video =
+  ( offset video.createdAt
+  , (offset video.createdAt) + video.duration
+  )
+  
+offset : Date -> Time
+offset date =
+  toFloat (0
+  + (Date.hour date) * 60 * 60
+  + (Date.minute date) * 60
+  + (Date.second date)
+  ) * 1000
