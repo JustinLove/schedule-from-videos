ec9ab8486aa7207216db27aefe989885891aae92
Justin Love <git@JustinLove.name>
2018-09-18 11:32:20 -0500

    use eternal icons now that elm replaces body wholesale

--- src/View.elm
+++ src/View.elm
@@ -131,7 +131,7 @@ displayFooter mode =
 icon : String -> Html msg
 icon name =
   svg [ Svg.Attributes.class ("icon icon-"++name) ]
-    [ use [ xlinkHref ("#icon-"++name) ] [] ]
+    [ use [ xlinkHref ("symbol-defs.svg#icon-"++name) ] [] ]
 
 targetValue : Json.Decode.Decoder a -> (a -> Msg) -> Json.Decode.Decoder Msg
 targetValue decoder tagger =
