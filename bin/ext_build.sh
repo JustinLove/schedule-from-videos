e9c4b3e01636693ba2042821718300128cfde38c
Justin Love <git@JustinLove.name>
2021-11-19 14:39:59 -0600

    exclude web html from extension package

--- bin/ext_build.sh
+++ bin/ext_build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 elm make src/ScheduleFromVideos.elm --output public/schedule-from-videos.js --optimize
 elm make src/Config.elm --output public/config/config.js --optimize
+cd public
+# don't ask me why we have to explicitly include config.js here
+/c/Program\ Files/7-Zip/7z a -r '-xr0!index.html' '-i!config/config.js' -bb3 ../releases/sfc.zip *.*
